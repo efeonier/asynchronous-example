26cde33003f2ce3c7efa5b97cbd39d9197d60a6c
Efe Önier <efe.onier@sendeo.com.tr>
2024-10-31 09:00:54 +0300

    ContinueWith

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+var myTask = new HttpClient().GetStringAsync("https://www.google.com/").ContinueWith((data) =>
+{
+    Console.WriteLine(data.Result.Length);
+    Console.WriteLine("Arada yapilacak isler");
+});
+
+await myTask;
